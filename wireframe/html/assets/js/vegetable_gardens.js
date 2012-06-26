931b0e225d26e494f773efe43b4ecea9f10479d6
Fernando Guillen <fguillen.mail@gmail.com>
2012-06-26 23:25:17 +0200

    HTML wireframe: details

--- wireframe/html/assets/js/vegetable_gardens.js
+++ wireframe/html/assets/js/vegetable_gardens.js
@@ -1,4 +1,5 @@
 $.urlParam = function(name){
     var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if( !results ) return null;
     return results[1] || 0;
 }
